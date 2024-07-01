8076ba3e2ffa79e639b57b70e873403c1e153137
Christoph Gorgulla <c.gorgulla@gmail.com>
2024-07-01 10:03:29 -0500

    removing prefix

--- tools/vfvs_prepare_atg-primary-screen-folders.sh
+++ tools/vfvs_prepare_atg-primary-screen-folders.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 #Checking the input arguments
-usage="Usage: vfvs_prepare_atg-primary-screen-folders.sh <jobname prefix> <size 1> <size 2> ...
+usage="Usage: vfvs_prepare_atg-primary-screen-folders.sh <size 1> <size 2> ...
 
 Description: Preparing the folders for the ATG Primary Screens folders. For each docking scenario, and each specified screening size, one ATG Primary Screen folder will be created. Before running this script, the todo files have to be prepared. 
 
 Arguments:
-    <jobname prefix>: String that is used as prefix in the job output folders, e.g. abl1-vs1
     <size N>: Number of ligands that should be screened in the ATG Primary Screen. Multiple sizes can be spcified if multiple ATG Primary Screens are planned to be run with different screening sizes.
 "
 
@@ -14,7 +13,7 @@ if [ "${1}" == "-h" ]; then
    echo -e "\n${usage}\n\n"
    exit 0
 fi
-if [ "$#" -le "1" ]; then
+if [ "$#" -le "0" ]; then
    echo -e "\nWrong number of arguments. At two arguments are required."
    echo -e "\n${usage}\n\n"
    echo -e "Exiting..."
@@ -23,7 +22,6 @@ fi
 
 # Initial setup
 cd ../output-files
-prefix=$1
 
 # Prepare next stage foldersparent_dir=$(basename $(dirname $(pwd)))
 for ds in $(cat ../workflow/config.json  | jq -r ".docking_scenario_names" | tr "," " " | tr -d '"\n[]' | tr -s " "); do
@@ -35,8 +33,8 @@ for ds in $(cat ../workflow/config.json  | jq -r ".docking_scenario_names" | tr 
 done
 for ds in $(cat ../workflow/config.json  | jq -r ".docking_scenario_names" | tr "," " " | tr -d '"\n[]' | tr -s " "); do
   for size in ${@:2}; do
-    echo "Edting the all.ctrl file of the ATG Primary Screen of docking scenrio ${ds} for ${size} ligands: Setting job_name to ${prefix}-atg-primaryscreen_${size}_${ds}"
-    sed -i "s/job_name=.*/job_name=${prefix}-atg-primaryscreen_${size}_${ds}/g" ../../atg-primaryscreen_${size}_${ds}/tools/templates/all.ctrl
+    echo "Edting the all.ctrl file of the ATG Primary Screen of docking scenrio ${ds} for ${size} ligands: Setting job_name to atg-primaryscreen_${size}_${ds}"
+    sed -i "s/job_name=.*/job_name=atg-primaryscreen_${size}_${ds}/g" ../../atg-primaryscreen_${size}_${ds}/tools/templates/all.ctrl
   done
 done
 for ds in $(cat ../workflow/config.json  | jq -r ".docking_scenario_names" | tr "," " " | tr -d '"\n[]' | tr -s " "); do
