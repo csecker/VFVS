aae26689d98f15d4c966c8c7c5cd035b4e1ca869
Christoph Gorgulla <c.gorgulla@gmail.com>
2024-11-13 17:05:59 -0600

    Troubleshooting code

--- tools/templates/create_todofile_atg-primaryscreen.py
+++ tools/templates/create_todofile_atg-primaryscreen.py
@@ -178,9 +178,9 @@ elif tranche_scoring_mode in ["tranche_min_score", "tranche_ave_score"]:
     while ligands_selected < ligand_count_aim:
         
         # Checking if next ligand collection matches regex
-        print("tranche_filter_regex: ", tranche_filter_regex, "df_library_collections: ", df_library_collections.iloc[i])
+        print("tranche_filter_regex: ", tranche_filter_regex, "df_library_collections: ", df_library_collections.iloc[i].Tranche)
         
-        if re.fullmatch(tranche_filter_regex, df_library_collections.iloc[i]):
+        if re.fullmatch(tranche_filter_regex, df_library_collections.iloc[i].Tranche):
             print("The tranche matches the tranche_filter_regex. Including tranche...")
             
             # Selecting next ligand collection
